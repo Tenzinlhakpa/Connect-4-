bf40d214d680ad671c124d5e8f4c91f8a206fac5
Tenzinlhakpa <162301492+Tenzinlhakpa@users.noreply.github.com>
2024-03-06 13:20:24 +1000

    Add files via upload

---
+++ tenzin a1.py
@@ -0,0 +1,24 @@
+# DO NOT modify or add any import statements
+from typing import Optional
+from a1_support import *
+
+# Name:Tenzin Lhakpa
+# Student Number: s4779250
+# ----------------
+
+# Write your classes and functions here
+
+def num_hours() -> float:
+    return 4.5
+
+def generate_initial_board() -> list[str]:
+    def is_column_full(column: str) -> bool:
+        def is_column_empty(column: str) -> bool:
+            def display_board(board: list[str]) -> None:
+                def main() -> None:
+                    """The main function"""
+                    pass
+                if __name__ == "__main__":
+                    main()
+
+
